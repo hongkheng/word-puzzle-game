42a05b8db252161133b9bdd1e6bb3dd3118a3ae3
yaphongkheng <hongkheng.yap@gmail.com>
2023-04-19 17:20:05 +0800

    feat: add category select & button UI

--- src/game/Start.tsx
+++ src/game/Start.tsx
@@ -1,10 +1,26 @@
-import { Box, Text, VStack } from "native-base";
+import { Box, Button, Select, Text, VStack } from "native-base";
+import { useState } from "react";
 
 function Start() {
+  const [category, setCategory] = useState("");
+
   return (
     <Box safeArea>
-      <VStack alignItems="center">
-        <Text fontSize="3xl">Start game</Text>
+      <VStack space="1/3" alignItems="center">
+        <Text fontSize="3xl">Word Puzzle Game</Text>
+        <Select
+          selectedValue={category}
+          minWidth="200"
+          accessibilityLabel="Choose Category"
+          placeholder="Choose Category"
+          mt={1}
+          onValueChange={(itemValue) => setCategory(itemValue)}
+        >
+          <Select.Item label="Solar System" value="solar" />
+          <Select.Item label="Sea animals" value="sea" />
+          <Select.Item label="Dinosaurs" value="dinosaurs" />
+        </Select>
+        <Button>Start game</Button>
       </VStack>
     </Box>
   );
